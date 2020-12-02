e73616e337e29b6f754e7b914fd744fd8200b6f0
rachelwadsworth-smith <rachel.wadsworthsmith@gmail.com>
2020-12-02 01:09:20 -0700

    added quotes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let ties = 0;
 function chooseWeapon(weapon) {
   rand = arr[Math.floor(Math.random() * 3)];
   let choice = document.getElementById("choice");
-  choice.innerHTML = `You chose ${weapon}, computer chose ${rand}.`;
+  choice.innerHTML = `You chose ${weapon}, Immortal Joe chose ${rand}.`;
   chooseResult(weapon, rand);
 }
 
@@ -16,19 +16,19 @@ function chooseResult(weapon, rand)  {
      weapon === 'boomerang' && rand === 'rock' ||
      weapon === 'thunderstick' && rand === 'boomerang') {
       let result = document.getElementById("result");
-      result.innerHTML = 'You are a winner!';
+      result.innerHTML = 'Freeeedommm!!';
      tally('win');
   }   
   else if(weapon === 'rock' && rand === 'boomerang' || 
           weapon === 'boomerang' && rand === 'thunderstick' ||
           weapon === 'thunderstick' && rand === 'rock') {
             let result= document.getElementById("result");
-            result.innerHTML = 'You loser!';
+            result.innerHTML = 'You Know, Hope Is A Mistake. If You Can't Fix What's Broken, You'll, Uhhhh...You'll Go Insane.';
     tally('lose'); 
   }   
   else {
     let result= document.getElementById("result");
-    result.innerHTML = 'You Tied!'; 
+    result.innerHTML = 'We share blood. We are not brothers.'; 
     tally('tie');
   }   
 }
